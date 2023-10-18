999af81ce5dffb54a2a440db9d90df3ebe293e53
OliverPineda <opineda@ltu.edu>
2023-10-18 15:37:01 -0400

    Added StartStopBtn and VideoPicBox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.VideoPicBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.VideoPicBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(341, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "StartStopBtn";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // VideoPicBox
+            // 
+            this.VideoPicBox.Location = new System.Drawing.Point(233, 95);
+            this.VideoPicBox.Name = "VideoPicBox";
+            this.VideoPicBox.Size = new System.Drawing.Size(298, 196);
+            this.VideoPicBox.TabIndex = 1;
+            this.VideoPicBox.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.VideoPicBox);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.VideoPicBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Button button1;
+        private PictureBox VideoPicBox;
     }
 }
