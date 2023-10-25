86de6413a005ae2ac8612d765cc581fe4b8e34d2
OliverPineda <opineda@ltu.edu>
2023-10-25 14:55:10 -0400

    Added System.IO.Ports packages and robot.cs file

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,14 @@
         private void InitializeComponent()
         {
             this.StartStopBtn = new System.Windows.Forms.Button();
-            this.VideoPicBox = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.VideoPicBox)).BeginInit();
+            this.VideoPictureBox = new System.Windows.Forms.PictureBox();
+            this.arduinoLED = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.VideoPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // StartStopBtn
             // 
-            this.StartStopBtn.Location = new System.Drawing.Point(341, 318);
+            this.StartStopBtn.Location = new System.Drawing.Point(233, 315);
             this.StartStopBtn.Name = "StartStopBtn";
             this.StartStopBtn.Size = new System.Drawing.Size(112, 29);
             this.StartStopBtn.TabIndex = 0;
@@ -43,26 +44,36 @@
             this.StartStopBtn.UseVisualStyleBackColor = true;
             this.StartStopBtn.Click += new System.EventHandler(this.StartStopBtn_Click);
             // 
-            // VideoPicBox
+            // VideoPictureBox
             // 
-            this.VideoPicBox.Location = new System.Drawing.Point(233, 95);
-            this.VideoPicBox.Name = "VideoPicBox";
-            this.VideoPicBox.Size = new System.Drawing.Size(298, 196);
-            this.VideoPicBox.TabIndex = 1;
-            this.VideoPicBox.TabStop = false;
+            this.VideoPictureBox.Location = new System.Drawing.Point(233, 95);
+            this.VideoPictureBox.Name = "VideoPictureBox";
+            this.VideoPictureBox.Size = new System.Drawing.Size(298, 196);
+            this.VideoPictureBox.TabIndex = 1;
+            this.VideoPictureBox.TabStop = false;
+            // 
+            // arduinoLED
+            // 
+            this.arduinoLED.Location = new System.Drawing.Point(401, 315);
+            this.arduinoLED.Name = "arduinoLED";
+            this.arduinoLED.Size = new System.Drawing.Size(151, 29);
+            this.arduinoLED.TabIndex = 2;
+            this.arduinoLED.Text = "ArduinoLED";
+            this.arduinoLED.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.VideoPicBox);
+            this.Controls.Add(this.arduinoLED);
+            this.Controls.Add(this.VideoPictureBox);
             this.Controls.Add(this.StartStopBtn);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.VideoPicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VideoPictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -70,6 +81,7 @@
         #endregion
 
         private Button StartStopBtn;
-        private PictureBox VideoPicBox;
+        private PictureBox VideoPictureBox;
+        private Button arduinoLED;
     }
 }
